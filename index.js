ec4bd7e1e14cdc2c3c1a5319420692ede76079eb
Timilehin Arigbede <Harriemannie@gmail.com>
2021-11-30 11:27:59 +0100

    Initial commit + MongoDB connection

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL).then(() => console.log("DB Connection Successful")).catch((err) => {
+  console.log(err);
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Backend server is running")
+})
