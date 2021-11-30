03f3e7a77cbd2854272b7180cb0cfa8f8ff34b30
Timilehin Arigbede <Harriemannie@gmail.com>
2021-11-30 12:46:11 +0100

    Add models

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 const dotenv = require("dotenv");
+const userRoute = require("./routes/user")
 
 dotenv.config();
 
 mongoose.connect(process.env.MONGO_URL).then(() => console.log("DB Connection Successful")).catch((err) => {
   console.log(err);
 });
+
+app.use(express.json());
+app.use("/api/users", userRoute)
 
 app.listen(process.env.PORT || 3000, () => {
   console.log("Backend server is running")
